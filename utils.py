5cdad55167fa68859765a7d0580811218db4b14a
Simon <simon.zabrocki@gmail.com>
2021-07-22 00:06:57 +0900

    update header

--- utils.py
+++ utils.py
@@ -38,7 +38,18 @@ def format_data(data):
 
 
 def Header(app, active_tab='Global Overview'):
-    return html.Div([get_header(app), html.Br([]), get_menu(active_tab)])
+    top_header = get_header(app)
+    menu = get_menu(active_tab)
+
+    page_title = html.Div(
+        [
+            html.Div([
+                page_menu(active_tab)],
+                     className='pretty_container twelve columns'),
+        ],
+        className="row")
+
+    return html.Div([top_header, html.Br([]), menu, page_title])
 
 
 def get_header(app):
@@ -82,25 +93,51 @@ def get_header(app):
         ],
         className="mainheader",
     )
+
     return header
+
+
+def get_page_title(active_tab):
+    pass
+
+
+def get_menu_old(active_tab):
+
+    name_link_list = [
+        {'label': 'Green Growth Index', 'value': '/SimulationDashBoard/global_overview'},
+        {'label': 'Simulation Tool', 'value': '/SimulationDashBoard/simulation'},
+        {'label': 'Evidence Library', 'value': '/SimulationDashBoard/models'},
+    ]
+
+    div_list = get_menu_div_list(name_link_list, active_tab=active_tab, active_style={
+                                 'text-decoration': 'none', 'color': '#2db29b'}, className="tab")
+
+    menu = html.Div(div_list, className="row all-tabs")
+    return menu
 
 
 def get_menu(active_tab):
 
+    tab_menu = {
+         'Global Overview': "Green Growth Index",
+         'Regional Outlook': "Green Growth Index",
+         'Country Profile': "Green Growth Index",
+         'Country Comparison': "Green Growth Index",
+         'Data': "Evidence Library",
+         'Models Overview': "Evidence Library",
+         'Simulation': "Simulation Tool"
+    }
+
     tabs_links = {
-        'Global Overview': "/SimulationDashBoard/global-overview",
-        'Regional Outlook': "/SimulationDashBoard/regional-outlouk",
-        'Country Profile': "/SimulationDashBoard/country-profile",
-        'Country Comparison': "/SimulationDashBoard/country-comparator",
-        'Data': "/SimulationDashBoard/data",
-        'Models Overview': "/SimulationDashBoard/models",
-        'Simulation': "/SimulationDashBoard/simulation"
+        'Green Growth Index': '/SimulationDashBoard/global_overview',
+        'Simulation Tool': '/SimulationDashBoard/simulation',
+        'Evidence Library': '/SimulationDashBoard/models',
     }
 
     menu_list = []
 
     for name, link in tabs_links.items():
-        if name == active_tab:
+        if name == tab_menu[active_tab]:
             tab = dcc.Link(
                 name,
                 href=link,
@@ -117,6 +154,57 @@ def get_menu(active_tab):
 
     menu = html.Div(menu_list, className="row all-tabs")
     return menu
+
+
+def page_menu(active_tab):
+    if active_tab in ['Global Overview', 'Regional Outlook', 'Country Profile', 'Country Comparison']:
+        return gg_index_menu(active_tab)
+    if active_tab in ['Models Overview', 'Data']:
+        return evidence_lib_menu(active_tab)
+    else:
+        return []
+
+
+def gg_index_menu(active_tab):
+    name_link_list = [
+        {'label': 'Global Overview', 'value': '/SimulationDashBoard/global_overview'},
+        {'label': 'Regional Outlook', 'value': '/SimulationDashBoard/regional-outlouk'},
+        {'label': 'Country Profile', 'value': '/SimulationDashBoard/country-profile'},
+        {'label': 'Country Comparison', 'value': '/SimulationDashBoard/country-comparator'},
+    ]
+    div_list = get_menu_div_list(name_link_list, active_tab, className='tab')
+
+    return html.Div(div_list, className="row all-tabs")
+
+
+def evidence_lib_menu(active_tab):
+    name_link_list = [
+        {'label': 'Models Overview', 'value': '/SimulationDashBoard/models'},
+        {'label': 'Data', 'value': '/SimulationDashBoard/data'},
+    ]
+    div_list = get_menu_div_list(name_link_list, active_tab,  className='tab')
+    #return html.Div(div_list, className="thirdtabmain")
+    return html.Div(div_list, className="row all-tabs")
+
+
+def get_menu_div_list(name_link_list, active_tab, className='thirdtabs', active_style={'background-color': '#0bb89c', 'color': 'white'}):
+    div_list = []
+
+    for name_link in name_link_list:
+        name = name_link['label']
+        link = name_link['value']
+
+        if name == active_tab:
+            div = html.Div(
+                [dcc.Link(html.Button(name, style=active_style), href=link)], className=className)
+
+        else:
+            div = html.Div(
+                [dcc.Link(html.Button(name), href=link)], className=className)
+
+        div_list.append(div)
+
+    return div_list
 
 
 def is_btn_clicked(btn_id):
