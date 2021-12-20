2c8d3503d1e29e1411d6463db62753222046ccbf
Simon <simon.zabrocki@gmail.com>
2021-12-20 17:04:50 +0100

    add confidence to country profile

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ server = app.server
 
 INDEX_YEAR = 2020
 
-data, indicator_data, indicator_properties, dimension_properties, ISO_options, missing_data = load_all_data(max_year=INDEX_YEAR)
+data, indicator_data, indicator_properties, dimension_properties, ISO_options, missing_data, index_confidence = load_all_data(max_year=INDEX_YEAR)
